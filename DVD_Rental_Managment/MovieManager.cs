6d28be71fc46b6cdec3a1bc9fd6794c88c31f880
UT01126 <ut01126tic2024@gmailcom>
2024-10-11 11:31:39 +0530

    create readmovie,updatemovie,deletemovie

--- DVD_Rental_Managment/MovieManager.cs
+++ DVD_Rental_Managment/MovieManager.cs
@@ -15,6 +15,45 @@ namespace DVD_Rental_Managment
             Movies.Add(movie);
             Console.WriteLine("Movie Add Successfully");
         }
+        public void ReadMovie()
+        {
+            if (Movies.Count == 0)
+            {
+                Console.WriteLine("Movie not avaliable");
+            }
+            Console.WriteLine("movie avaliable");
+
+            foreach (Movie movie in Movies)
+            {
+                Console.WriteLine(movie);
+
+            }
+
+        }
+
+        public void UpdateMovie(string newmovieid, string newtitle, string newdirector, string newrentalPrice)
+        {
+            var movie = Movies.FirstOrDefault(m => m.movieID == newmovieid);
+            if (movie != null)
+            {
+                Movies.Add(movie);
+                Console.WriteLine(" movie is update");
+
+            }
+            Console.WriteLine("movie is not update");
+        }
+
+        public void Deletemovie(string newmovieid)
+        {
+            var movie = Movies.FirstOrDefault(m => m.movieID == newmovieid);
+            if (movie != null)
+            {
+                Movies.Remove(movie);
+                Console.WriteLine("movie deleted");
+
+            }
+            Console.WriteLine("not avaliable");
+        }
 
     }
 }
