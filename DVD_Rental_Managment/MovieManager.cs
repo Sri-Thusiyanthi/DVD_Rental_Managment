05c347889d729d7212772a910b960c4869616def
UT01126 <ut01126tic2024@gmailcom>
2024-10-11 11:30:20 +0530

    create movie

--- DVD_Rental_Managment/MovieManager.cs
+++ DVD_Rental_Managment/MovieManager.cs
@@ -8,7 +8,6 @@ namespace DVD_Rental_Managment
 {
     internal class MovieManager
     {
-
         public List<Movie> Movies = new List<Movie>();
 
         public void CreateMovie(Movie movie)
@@ -17,45 +16,5 @@ namespace DVD_Rental_Managment
             Console.WriteLine("Movie Add Successfully");
         }
 
-        public void ReadMovie()
-        {
-            if (Movies.Count == 0)
-            {
-                Console.WriteLine("Movie not avaliable");
-            }
-            Console.WriteLine("movie avaliable");
-
-            foreach (Movie movie in Movies)
-            {
-                Console.WriteLine(movie);
-
-            }
-
-        }
-
-        public void UpdateMovie(string newmovieid,string newtitle,string newdirector,string newrentalPrice)
-        {
-            var movie = Movies.FirstOrDefault(m => m.movieID == newmovieid);
-            if (movie != null)
-            {
-                Movies.Add(movie);
-                Console.WriteLine(" movie is update");
-            
-            }
-            Console.WriteLine("movie is not update");
-        }
-
-        public void Deletemovie(string newmovieid)
-        {
-            var movie = Movies.FirstOrDefault(m => m.movieID == newmovieid);
-            if (movie != null) 
-            {
-             Movies.Remove(movie);
-                Console.WriteLine("movie deleted");
-            
-            }
-            Console.WriteLine("not avaliable");
-        }
-        
     }
 }
